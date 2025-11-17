358b6ef105a93fd5be466447172343ec2d99ae72
jeffreynetten <74904321+jeffreynetten@users.noreply.github.com>
2025-11-17 18:16:19 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+const hamMenu = document.querySelector('header button');
+
+const offScreen = document.querySelector('header ul:nth-of-type(2)')
+
+hamMenu.addEventListener('click' , () => {
+    hamMenu.classList.toggle('active');
+    offScreen.classList.toggle('active');
+})
